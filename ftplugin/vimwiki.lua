4e92ee136b5c7b6edd2d9cea0ff2c812fee0c88c
RobbieClarken <robbie.clarken@gmail.com>
2026-08-31 11:33:32 +0930

    Improve navigation in anthropic request log files

--- ftplugin/vimwiki.lua
+++ ftplugin/vimwiki.lua
@@ -5,3 +5,11 @@ vim.keymap.set(
   "copilot#Accept('<c-r>=vimwiki#tbl#kbd_tab()<cr>')",
   { buffer = 0, silent = true, expr = true, replace_keycodes = false }
 )
+
+-- Let % jump between XML-ish tag pairs (e.g. <request> ... </request>).
+-- The markdown ftplugin gets this from ftplugin/html.vim, but vimwiki doesn't.
+vim.b.match_ignorecase = false
+vim.b.match_words = table.concat({
+  [[<!--:-->]],
+  [[<\@<=\([^/!][^ \t>]*\)[^>]*\%(>\|$\):<\@<=/\1>]],
+}, ',')
--- lua/custom/plugins/vimwiki.lua
+++ lua/custom/plugins/vimwiki.lua
@@ -5,7 +5,7 @@ vim.g.vimwiki_list = { { path = '~/Documents/notes/', syntax = 'markdown', ext =
 -- Disable vimwiki's insert mode tab key mapping because it blocks the use of tab for
 -- copilot suggestion acceptance. We re-enable tab for table cell navigation in a
 -- copilot compatible way in ftplugin/vimwiki.lua.
-vim.g.vimwiki_key_mappings = { table_mappings = 0 }
+vim.g.vimwiki_key_mappings = { headers = 0, table_mappings = 0 }
 
 -- Prevent wikiwiki from creating a local `diary` folder when keymaps are run from inside
 -- a markdown file:
