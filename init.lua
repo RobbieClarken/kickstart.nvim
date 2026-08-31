41aac6ef36e81e3ea5098bcdd10feaa87070d621
RobbieClarken <robbie.clarken@gmail.com>
2026-08-31 11:37:18 +0930

    Add light mode colour scheme

--- init.lua
+++ init.lua
@@ -352,10 +352,22 @@ do
     },
   })
 
+  vim.pack.add({ gh('projekt0n/github-nvim-theme') })
+  require('github-theme').setup({})
+
+  local function macos_dark()
+    local out = vim.fn.system({ 'defaults', 'read', '-g', 'AppleInterfaceStyle' })
+    return vim.v.shell_error == 0 and out:match('Dark') ~= nil
+  end
+
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme('tokyonight-night')
+  if macos_dark() then
+    vim.cmd.colorscheme('tokyonight-night')
+  else
+    vim.cmd.colorscheme('github_light')
+  end
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add({ gh('folke/todo-comments.nvim') })
